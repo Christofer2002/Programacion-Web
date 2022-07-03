8da8ab1b286d1043a202019a52d715cd031f828f
Christofer Daniel Chaves Pauth <cristoferchaves36@gmail.com>
2022-07-03 16:15:44 -0600

    Pagina web actualizada

--- js/carrusel.js
+++ js/carrusel.js
@@ -13,16 +13,16 @@ $(function(){
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: 3,
+                slidesToShow: 2,
                 slidesToScroll: 1,
                 infinite: true,
-                dots: true
+                /*dots: true*/
               }
             },
             {
               breakpoint: 600,
               settings: {
-                slidesToShow: 2,
+                slidesToShow: 1,
                 slidesToScroll: 2
               }
             },
